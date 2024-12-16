914add4887fe5c15e2abedcfcd187516d0ee2ecd
Daniel Bengl <daniel.bengl@renuo.ch>
2024-12-16 21:39:07 +0100

    Format

--- index.ts
+++ index.ts
@@ -3,53 +3,53 @@ import { config } from 'dotenv';
 config();
 
 const API_URL = 'https://gifcoins.io/api/v1/transactions';
-const API_KEY = process.env.API_KEY;  
+const API_KEY = process.env.API_KEY;
 const NAME_FILTER = process.env.FILTER_NAME;
 
 async function fetchTransactions() {
-    let allTransactions = [];
-    let hasMorePages = true;
-    let page = 1;
+  let allTransactions = [];
+  let hasMorePages = true;
+  let page = 1;
 
-    while (hasMorePages) {
-        const url = `${API_URL}?page=${page}`
-        console.log(`Fetching transactions from ${url}...`)
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'X-Api-Key': API_KEY,
-                'Content-Type': 'application/json',
-            }
-        });
+  while (hasMorePages) {
+    const url = `${API_URL}?page=${page}`
+    console.log(`Fetching transactions from ${url}...`)
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'X-Api-Key': API_KEY,
+        'Content-Type': 'application/json',
+      }
+    });
 
-        if (!response.ok) {
-            console.error('Failed to fetch transactions:', response.statusText);
-            return [];
-        }
-
-        const data = await response.json();
-        const transactions = data.transactions || [];
-        allTransactions.push(...transactions);
-
-        const pages = data.pagination.pages;
-
-        if (++page >= pages) {
-            hasMorePages = false;
-        }
+    if (!response.ok) {
+      console.error('Failed to fetch transactions:', response.statusText);
+      return [];
     }
 
-    return allTransactions;
+    const data = await response.json();
+    const transactions = data.transactions || [];
+    allTransactions.push(...transactions);
+
+    const pages = data.pagination.pages;
+
+    if (++page >= pages) {
+      hasMorePages = false;
+    }
+  }
+
+  return allTransactions;
 }
 
 async function main() {
-    const transactions = await fetchTransactions();
-    console.log(`Fetched ${transactions.length} transactions.`);
+  const transactions = await fetchTransactions();
+  console.log(`Fetched ${transactions.length} transactions.`);
 
-    const filteredTransactions = transactions.filter(transaction =>
-        transaction.spender.name === NAME_FILTER  || transaction.recipient.name === NAME_FILTER 
-    );
+  const filteredTransactions = transactions.filter(transaction =>
+    transaction.spender.name === NAME_FILTER || transaction.recipient.name === NAME_FILTER
+  );
 
-    console.log('Filtered Transactions:', filteredTransactions);
+  console.log('Filtered Transactions:', filteredTransactions);
 }
 
 main().catch(error => console.error('Error:', error));
