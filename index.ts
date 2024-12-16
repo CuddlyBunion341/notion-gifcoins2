238babfb648dd1ff6ff1a0dd1fa59b5ab1669b78
Daniel Bengl <daniel.bengl@renuo.ch>
2024-12-16 21:48:30 +0100

    Implement progress bar

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
-import { config } from 'dotenv';
+import { config } from 'dotenv'
+import cliProgress from 'cli-progress'
+import colors from 'ansi-colors'
 
 config();
 
@@ -6,14 +8,21 @@ const API_URL = 'https://gifcoins.io/api/v1/transactions';
 const API_KEY = process.env.API_KEY;
 const NAME_FILTER = process.env.FILTER_NAME;
 
+const b1 = new cliProgress.SingleBar({
+    format: 'Fetching Gifcoins |' + colors.yellow('{bar}') + '| {percentage}% || {value}/{total} Transactions',
+    barCompleteChar: '\u2588',
+    barIncompleteChar: '\u2591',
+    hideCursor: true
+})
+
 async function fetchTransactions() {
   let allTransactions = [];
   let hasMorePages = true;
   let page = 1;
+  let isFirstRequest = true;
 
   while (hasMorePages) {
     const url = `${API_URL}?page=${page}`
-    console.log(`Fetching transactions from ${url}...`)
     const response = await fetch(url, {
       method: 'GET',
       headers: {
@@ -33,10 +42,20 @@ async function fetchTransactions() {
 
     const pages = data.pagination.pages;
 
+    if (isFirstRequest) {
+      b1.start(pages, 0)
+      isFirstRequest = false
+    }
+
+    b1.increment()
+
+
     if (++page >= pages) {
       hasMorePages = false;
     }
   }
+
+  b1.stop()
 
   return allTransactions;
 }
